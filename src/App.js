042bf9f9ee29e836f5a0ab388bb845be0f970c33
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-10-13 17:19:41 +0530

    before merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import react , {useEffect} from 'react'
 import './App.css';
-import axios from 'axios'
+// import axios from 'axios'
 
 function App() {
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/student/UnderGraduates/')
-    .then(resp=>{
-      console.log(resp.data)
+  useEffect(()=>{
+    fetch('http://localhost:8000/api/student/UnderGraduates/', {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
     })
+      .then(response => response.json())
+      .then(data => 
+        console.log(data))
   },[])
+
   return (
     <div className="App">
         welcome
     </div>
   );
+
 }
 
 export default App;
