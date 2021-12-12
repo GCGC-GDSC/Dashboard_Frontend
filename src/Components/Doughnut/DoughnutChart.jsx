b5204ef13c7b3dde128d32b14cc5a4f63ccc46ba
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-12-12 21:19:32 +0530

    Styled Doughnut, dynamically imported data to support population of next charts

--- src/Components/Doughnut/DoughnutChart.jsx
+++ src/Components/Doughnut/DoughnutChart.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const DoughnutChart = ({data,options}) => {
+const DoughnutChart = ({data,options,setUnlockVC}) => {
   return(
   <>
     <div className='header'>
