c69c06d49bf329db38580a10f835c94caf7a2611
Guru Charan <guru.134charan@gmail.com>
2022-01-25 15:22:18 +0530

    ui changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,10 @@
 import React, { useEffect } from 'react'
-import DoughnutChart from "../../Components/Doughnut/DoughnutChart"
-import Fetcher from '../../Components/Fetcher/Fetcher'
-import Fetcher2 from '../../Components/Fetcher2/Fetcher2'
-import HorizontalBarChart from '../../Components/HorizontalBar/HorizontalBar'
-import ParticlesComponent from '../../Components/Particles/ParticlesComponent'
-import PieChart from "../../Components/PieChart/PieChart"
-import VerticalBar from '../../Components/VerticalBar/VerticalBarChart'
 import "./Home.scss"
-import ParticleBackground from '../../ParticleBackground'
 import HeroText from "../../Components/HeroText/HeroText"
 import Aos from "aos"
 import "aos/dist/aos.css"
 import Footer from '../../Components/Footer/Footer'
-import Overall from '../../Views/Overall/Overall'
 import BasicTabs from "../../Components/Tabs/Tabs"
-import Login from '../../Components/Login/Login.component'
 // import Assistant from "../../Components/Assistant/Assistant"
 
 function Home({user}) {
