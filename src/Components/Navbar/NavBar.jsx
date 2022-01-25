7392f169138e8f434641888c93ca995ce371ba14
Krishna Chaitanya Kadali <121910302022@gitam.in>
2022-01-25 15:42:13 +0530

    .

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -34,9 +34,9 @@ function NavBar({user}) {
             <NavLink className='navbar-link' activeStyle={selected}  to = '/admin'>
                 Admin
             </NavLink>
-            <button className='navbar-link' onClick={()=>firebase.auth().signOut()}>
+            {/* <button className='navbar-link' onClick={()=>firebase.auth().signOut()}>
                 Logout
-            </button>
+            </button> */}
             </div>:
             <NavLink className='navbar-link' activeStyle={selected}  to = '/'>
                 Login
