36499fd7ecbb0aa32a0240fe1ad3d80947b28edf
Guru Charan <guru.134charan@gmail.com>
2022-01-31 01:00:46 +0530

    setup teams page, connected with firebase, ui changes for cards component

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,15 +1,35 @@
 import ProfileCard from "./profileCard"
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import {firestore} from "../../backend/firebase.config"
 import './Team.style.scss'
+import { useEffect, useState } from "react";
 const Team = () => {
+  const [members,setMembers] = useState([])
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  useEffect(()=>{
+    firestore.collection("members").get()
+    .then(resp=>{
+      const arr =[]
+       resp.docs.map(item=>arr.push(item.data()))
+       setMembers(shuffle(arr))
+    })
+  },[])
   return <>
   <div style={{marginTop:"100px"}}></div>
   <div className="profile-cards-container">
-      <ProfileCard/>
-      <ProfileCard/>
-      <ProfileCard/>
-      <ProfileCard/>
+    {members.map(member=>
+      <ProfileCard member={member} key={member.rollnumber}/>
+      )}
   </div>
   </>
 }
