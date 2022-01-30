434ffe47d440d1492b653173ba4a8f5eac5891d8
Krishna Chaitanya Kadali <121910302022@gitam.in>
2022-01-31 01:02:56 +0530

    firebase for cards

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,8 +1,17 @@
 import ProfileCard from "./profileCard"
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import {firebase} from 'firebase';
 import './Team.style.scss'
 const Team = () => {
+  
+firebase.collections("users").set({
+  name:"Krishna Chaitanya",
+  role:"FrontEnd",
+  rollnumber:"121910302022",
+  description:"cement se nikla gulab ithihass ek kitab har halka jawab sach hua mera kvaab wafadar kabardar hoshiyaar gully gang sabse hard parivar",
+})
+
   return <>
   <div style={{marginTop:"100px"}}></div>
   <div className="profile-cards-container">
