c1022c73f7256b77764cbdbd43e2e978a7c73063
Krishna Chaitanya Kadali <121910302022@gitam.in>
2022-02-02 00:45:02 +0530

    soo close to version 1git add .

--- src/Pages/Admin/StudentDetailsFormObj.js
+++ src/Pages/Admin/StudentDetailsFormObj.js
@@ -1,32 +1,38 @@
 const studentDetailsRef = [
   "under_campus_name",
   "under_institute_name",
-  "is_ug",   
+  "is_ug",
   "total_students",
   "total_final_years",
-  "total_higher_study_and_pay_crt","total_not_intrested_in_placments","total_backlogs",
-  "total_offers","total_multiple_offers","highest_salary"
-  ,"average_salary" ,"lowest_salary"
- 
+  "total_higher_study_and_pay_crt",
+  "total_not_intrested_in_placments",
+  "total_backlogs",
+  "total_offers",
+  "total_multiple_offers",
+  "highest_salary",
+  "average_salary",
+  "lowest_salary",
 ];
 
 export const parsedStudentDetailsRef = {
-  "under_campus_name":"Campus Name",
-  "under_institute_name":"Select the Institute",
-  "is_ug":"Is it Undergraduate",
-  "total_students":"Number of students",
-  "total_final_years": "Number of Final Year Students",
-  "total_higher_study_and_pay_crt": "Number of students that are choosing higher studies",
-  "total_not_intrested_in_placments": "Number of students intrested in placements",
-  "total_backlogs":"Backlogs count",
-  "total_offers": "Offers Count",
-  "total_multiple_offers": "Number of Multiple Offers",
-  "highest_salary":"What is the Highest Salary",
-  "average_salary" :"What is the average salary",
-  "lowest_salary":"What is the Lowest Salary",
-  "vskp":"Visakhapatnam",
-  "blr":"Bangaluru",
-  "hyd":"Hyderabad"
-}
+  under_campus_name: "Campus Name",
+  under_institute_name: "Select the Institute",
+  is_ug: "Is it Undergraduate",
+  total_students: "Number of students",
+  total_final_years: "Number of Final Year Students",
+  total_higher_study_and_pay_crt:
+    "Number of students that are choosing higher studies",
+  total_not_intrested_in_placments:
+    "Number of students intrested in placements",
+  total_backlogs: "Backlogs count",
+  total_offers: "Total No. of Offers",
+  total_multiple_offers: "No. of multiple offers",
+  highest_salary: "Highest (Per annum) Rs.in lakhs",
+  average_salary: "Average (Per annum)Rs.in lakhs",
+  lowest_salary: "Lowest (Per annum) Rs.in lakhs",
+  vskp: "Visakhapatnam",
+  blr: "Bengaluru",
+  hyd: "Hyderabad",
+};
 
 export default studentDetailsRef;
