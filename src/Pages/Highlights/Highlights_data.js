fcb1b7aee654df0cc3f08912d78eacf0d8643d69
Krishna Chaitanya Kadali <121910302022@gitam.in>
2022-09-15 10:59:44 +0530

    json data to load highlights

---
+++ src/Pages/Highlights/Highlights_data.js
@@ -0,0 +1,38 @@
+export const highlights_data = [
+  {
+    year: "AY-2022-23",
+    Companies: 140,
+    PlacementOffers: 1799,
+    Highestpackage: 44,
+    OfPlacements: 86.33,
+  },
+  {
+    year: "AY-2021-22",
+    Companies: 150,
+    PlacementOffers: 1899,
+    Highestpackage: 66,
+    OfPlacements: 86.33,
+  },
+
+  {
+    year: "AY-2021-20",
+    Companies: 160,
+    PlacementOffers: 1299,
+    Highestpackage: 15,
+    OfPlacements: 86.33,
+  },
+  {
+    year: "AY-2020-21",
+    Companies: 170,
+    PlacementOffers: 1399,
+    Highestpackage: 11,
+    OfPlacements: 86.33,
+  },
+  {
+    year: "AY-2019-20",
+    Companies: 180,
+    PlacementOffers: 1499,
+    Highestpackage: 77,
+    OfPlacements: 86.33,
+  },
+];
